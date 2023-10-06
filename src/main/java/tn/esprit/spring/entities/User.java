992684645542157bc85b19e5942aff0fc529a5d9
ameni56 <ameni.hajri@esprit.tn>
2023-10-06 16:05:57 +0100

    modification 2 branch userManagmenet

--- src/main/java/tn/esprit/spring/entities/User.java
+++ src/main/java/tn/esprit/spring/entities/User.java
@@ -14,6 +14,7 @@ import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 //Test
+//Test2
 @Entity
 @Table(name = "T_USER")
 public class User implements Serializable {
